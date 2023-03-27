7b5425f39f39822b3f4a080f837e4573f5eecd58
tsripramong <126552401+tsripramong@users.noreply.github.com>
2023-03-27 23:20:29 +0700

    Update vga256.h

--- F411_VGA256/Core/Inc/vga256.h
+++ F411_VGA256/Core/Inc/vga256.h
@@ -1,8 +1,24 @@
-/*
- * vga256.h
- *
- *  Created on: Feb 16, 2023
- *      Author: thanwa
+/**
+ * original author:  Tilen Majerle<tilen@majerle.eu>
+ * modification for STM32f10x: Alexander Lutsai<s.lyra@ya.ru>
+ * modification for STM32F103: for 8-bit color VGA thanwa (2023)
+   ----------------------------------------------------------------------
+   	Copyright (C) Alexander Lutsai, 2016
+    Copyright (C) Tilen Majerle, 2015
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+     
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   ----------------------------------------------------------------------
  */
 #include <stdint.h>
 #include "vgafonts.h"
